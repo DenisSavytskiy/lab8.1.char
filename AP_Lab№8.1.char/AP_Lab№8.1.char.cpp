c5f335f3f5ac6df6915fa870dbbc48d2e2ab8e4b
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-30 02:42:41 +0200

    first commit

---
+++ AP_Lab№8.1.char/AP_Lab№8.1.char.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int Count(char* s) {
+    int k = 0, pos = 0;
+    char* t;
+    while (t = strstr(s + pos, "while")) {
+        k++;
+        pos = t - s + 1;
+    }
+    return k;
+}
+
+char* Change(char* s) {
+    size_t len = strlen(s);
+    char* t = new char[len * 2];
+    char* p;
+    int pos1 = 0, pos2 = 0;
+    t[0] = '\0';
+
+    while (p = strstr(s + pos1, "while")) {
+        pos2 = p - s + 5;
+        strncat_s(t, len * 2, s + pos1, p - s - pos1);
+        strcat_s(t, len * 2, "***");
+        pos1 = pos2;
+    }
+    strcat_s(t, len * 2, s + pos1);
+    strcpy_s(s, len * 2, t);
+    return t;
+}
+
+int main()
+{
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+    cout << "String contained " << Count(str) << " groups of 'while'" << endl;
+    char* dest = new char[151];
+    dest = Change(str);
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << dest << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№8.1.char/AP_Lab№8.1.char.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1 {
+    TEST_CLASS(CharArrayTests) {
+public:
+    TEST_METHOD(TestCharCount) {
+        char str[] = "while doing while loop while";
+        int result = Count(str);
+        Assert::AreEqual(3, result);
+    }
+
+    TEST_METHOD(TestCharChange) {
+        char str[100] = "while doing while loop while";
+        char* result = Change(str);
+        Assert::IsTrue(strcmp("*** doing *** loop ***", result) == 0);
+        delete[] result;
+    }
+    };
+}
